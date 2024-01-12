28e2c4a3129b00ab9806ff2a967f34275af9d59d
Sahil <garg07825@gmail.com>
2024-01-13 05:24:53 +0530

    Project basix setup is ready with vite and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-2xl font-solid text-red-500">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
