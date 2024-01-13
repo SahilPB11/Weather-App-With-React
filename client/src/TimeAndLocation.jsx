ddc03f949fbe71a8fefbbdd6a8c87875db0c476e
Sahil <garg07825@gmail.com>
2024-01-13 06:18:55 +0530

    TimeAndLocation is completed

---
+++ client/src/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <div className="text-white text-xl font-extralight">
+          Tuesday31 may 2022 | Local Time: 12:46 PM
+        </div>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Berlin, De</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
