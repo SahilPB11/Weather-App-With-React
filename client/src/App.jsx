511d53b53d48bf5861ee7698414c6a3a59aa9bc6
Sahil <garg07825@gmail.com>
2024-01-13 05:38:40 +0530

    top bar is ready with burrons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
 import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1 className="text-2xl font-solid text-red-500">Hello world</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32  bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
     </div>
   );
 }
